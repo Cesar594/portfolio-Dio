e03be5531b6673a0eca38e215330b01810b9dd21
Cesar594 <julio594jcs@gmail.com>
2025-06-25 19:03:18 -0300

    atualização

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Cesar594/Js-Developer-portfolio-Dio/refs/heads/main/portf%C3%B3lio%20atualizado%20Dio/js-developer-portfolio/data/profile.json';
+    const url =
+      "https://raw.githubusercontent.com/Cesar594/portfolio-Dio/refs/heads/main/data/profile.json";
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
